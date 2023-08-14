319b4a068999de32966a1368b5351d9baf7c760e
kenji nakamura <k-nakamura@egstock.co.jp>
2023-08-14 11:11:11 +0900

    fix yml & test.tsx

--- my-remix-app/app/routes/test.tsx
+++ my-remix-app/app/routes/test.tsx
@@ -1,11 +1,9 @@
-export default function test() {
+export default function Index() {
     return (
-        <div className={'grid grid-cols-12 mt-10'}>
-            <div className={'col-span-4'} />
-            <div className={'col-span-4 bg-blue-100 w-[100px] text-center text-red-500 font-bold'}>
+        <div className={'grid place-items-center mt-10'}>
+            <div className={'bg-blue-100 w-[100px] text-center text-red-500 font-bold'}>
                 <h1>Hello, test</h1>
             </div>
-            <div className={'col-span-4'} />
         </div>
     )
 }
